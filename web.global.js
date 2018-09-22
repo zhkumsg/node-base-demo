064fcdc03a34857448a4bdabadcc3464e844c62f
MoShengGui <12547665721@qq.com>
2018-09-22 11:24:38 +0800

    global

--- web.global.js
+++ web.global.js
@@ -11,7 +11,6 @@ const Webglobal = {
         global["SYS_PARAMINFO"] = [];
         client.Query(QueryModel.ZK_PARAMINFO, [], null, 0, 0, false, null).then(m => {
             global["SYS_PARAMINFO"] = m.result;
-            console.log(global["SYS_PARAMINFO"]);
         }).catch(err => {
             console.log("获取系统参数异常", err);
         });
