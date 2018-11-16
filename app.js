6110f4a6c985e487f3f410b4f6a340465d8db79b
君海PC <moshenggui@ijunhai.com>
2018-11-16 19:50:31 +0800

    日志

--- app.js
+++ app.js
@@ -1,67 +1,85 @@
-var ejs = require('ejs');
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var TokenHelper = require('./common/TokenHelper');
+var ejs = require("ejs");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var TokenHelper = require("./common/TokenHelper");
 
-var usersRouter = require('./routes/users');
-var startRouter = require('./routes/start');
-var homeRouter = require('./routes/home');
-var mainRouter = require('./routes/main');
-var investorRouter = require('./routes/investor');
-var investmentRouter = require('./routes/investment');
-var paramPouter = require('./routes/param');
-var permitconfigRouter = require('./routes/permitconfig');
-var permitRouter = require('./routes/permit');
-var roleRouter = require('./routes/role');
+var usersRouter = require("./routes/users");
+var startRouter = require("./routes/start");
+var homeRouter = require("./routes/home");
+var mainRouter = require("./routes/main");
+var investorRouter = require("./routes/investor");
+var investmentRouter = require("./routes/investment");
+var paramPouter = require("./routes/param");
+var permitconfigRouter = require("./routes/permitconfig");
+var permitRouter = require("./routes/permit");
+var roleRouter = require("./routes/role");
 
 var app = express();
-
+var fs = require("fs");
+var FileStreamRotator = require("file-stream-rotator");
 
 //允许跨域(适合token方式，跨域无法携带cookie)
-app.all('*', function (req, res, next) {
+app.all("*", function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Expose-Headers", "access-token");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,access-token,permit-token");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,Content-Type,access-token,permit-token"
+  );
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", " 3.2.1");
   res.header("Content-Type", "application/json;charset=utf-8");
-  if (req.headers["access-token"] && req.method.toLocaleUpperCase() !== "OPTIONS") {
-    TokenHelper.get(req.headers["access-token"]).then(user => {
-      delete user.iat;
-      delete user.exp;
-      req["UserInfo"] = user;
-      if (user && !(/logout/gi.test(req.url))) {
-        res.header("access-token", TokenHelper.set(user));
-      }
-      TokenHelper.get(req.headers["permit-token"]).then(permit => {
-        delete permit.iat;
-        delete permit.exp;
-        req["UserPermit"] = permit;
+  if (
+    req.headers["access-token"] &&
+    req.method.toLocaleUpperCase() !== "OPTIONS"
+  ) {
+    TokenHelper.get(req.headers["access-token"])
+      .then(user => {
+        delete user.iat;
+        delete user.exp;
+        req["UserInfo"] = user;
+        if (user && !/logout/gi.test(req.url)) {
+          res.header("access-token", TokenHelper.set(user));
+        }
+        TokenHelper.get(req.headers["permit-token"]).then(permit => {
+          delete permit.iat;
+          delete permit.exp;
+          req["UserPermit"] = permit;
+          next();
+        });
+      })
+      .catch(() => {
         next();
       });
-    }).catch(() => {
-      next();
-    });
   } else {
     next();
   }
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.engine('html', ejs.__express);
-app.set('view engine', 'html');
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", ejs.__express);
+app.set("view engine", "html");
 
-app.use(logger('dev'));
+//日志设置
+var logDirectory = path.join(__dirname, "logs");
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+var accessLogStream = FileStreamRotator.getStream({
+  date_format: "YYYYMMDD",
+  filename: path.join(logDirectory, "access-%DATE%.log"),
+  frequency: "daily",
+  verbose: false
+});
+
+app.use(logger("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //路由配置(写成.ashx是历史遗留问题)
 app.use("/Http/Msg/MsgStart.ashx", startRouter);
@@ -72,23 +90,23 @@ app.use("/Http/Msg/InvestmentServer.ashx", investmentRouter);
 app.use("/Http/Msg/ParamServer.ashx", paramPouter);
 app.use("/Http/Msg/PermitconfigServer.ashx", permitconfigRouter);
 app.use("/Http/Msg/PermitServer.ashx", permitRouter);
-app.use("/Http/Msg/RoleServer.ashx", roleRouter)
-app.use('/Http/Msg/UserServer.ashx', usersRouter);
+app.use("/Http/Msg/RoleServer.ashx", roleRouter);
+app.use("/Http/Msg/UserServer.ashx", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
