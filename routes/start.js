3b14bad43e3cc73f4412e29d11307f36f3bc032a
君海PC <moshenggui@ijunhai.com>
2018-11-27 18:09:50 +0800

    登录添加图片验证码

--- routes/start.js
+++ routes/start.js
@@ -1,35 +1,40 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const client = require("../common/ServiceClient");
-const MsgJsonHelper = require("../common/MsgJsonHelper");
-const QueryModel = require("../common/QueryModel");
-const MemoryCondition = require("../common/MemoryCondition");
-const { MType, MLogic, MOperator } = require("msg-dataaccess-base");
-const TokenHelper = require("../common/TokenHelper");
+const client = require('../common/ServiceClient');
+const MsgJsonHelper = require('../common/MsgJsonHelper');
+const QueryModel = require('../common/QueryModel');
+const MemoryCondition = require('../common/MemoryCondition');
+const { MType, MLogic, MOperator } = require('msg-dataaccess-base');
+const TokenHelper = require('../common/TokenHelper');
+var svgCaptcha = require('svg-captcha');
+const jsmd5 = require('js-md5');
 
-router.post("/", function(req, res, next) {
-  switch (req.query["method"]) {
-    case "Backend_Login":
-      Login(req, res);
-      break;
-    case "Backend_Logout":
-      Logout(req, res);
-      break;
-    default:
-      res.json(MsgJsonHelper.DebugJson("接口请求错误"));
-      break;
-  }
+router.post('/', function(req, res, next) {
+	switch (req.query['method']) {
+		case 'Backend_Login':
+			Login(req, res);
+			break;
+		case 'Backend_Logout':
+			Logout(req, res);
+			break;
+		default:
+			res.json(MsgJsonHelper.DebugJson('接口请求错误'));
+			break;
+	}
 });
 
-router.get("/", function(req, res, next) {
-  switch (req.query["method"]) {
-    case "Backend_Verifylogin":
-      Verifylogin(req, res);
-      break;
-    default:
-      res.json(MsgJsonHelper.DebugJson("接口请求错误"));
-      break;
-  }
+router.get('/', function(req, res, next) {
+	switch (req.query['method']) {
+		case 'Backend_Verifylogin':
+			Verifylogin(req, res);
+			break;
+		case 'Backend_Captcha':
+			Createcaptcha(req, res);
+			break;
+		default:
+			res.json(MsgJsonHelper.DebugJson('接口请求错误'));
+			break;
+	}
 });
 
 /**
@@ -38,42 +43,47 @@ router.get("/", function(req, res, next) {
  * @param {*} res
  */
 function Login(req, res) {
-  let id = req.body.ID;
-  let pwd = req.body.PWD;
-  if (!id || !pwd) {
-    return res.json(MsgJsonHelper.DebugJson("参数异常"));
-  }
-  let condition = [];
-  condition.push(
-    new MemoryCondition({
-      Field: "ZK_ID",
-      Logic: MLogic.And,
-      Operator: MOperator.Equal,
-      Type: MType.Mstring,
-      value: id
-    })
-  );
-  condition.push(
-    new MemoryCondition({
-      Field: "ZK_PASSWORD",
-      Logic: MLogic.And,
-      Operator: MOperator.Equal,
-      Type: MType.Mstring,
-      value: pwd
-    })
-  );
-  client
-    .Query(QueryModel.ZK_USERINFO, condition, null, 0, 0, false, null)
-    .then(m => {
-      if (m.result.length === 0) {
-        res.json(MsgJsonHelper.DebugJson("账号密码错误"));
-      } else {
-        VolidRole(m.result[0], res);
-      }
-    })
-    .catch(err => {
-      res.json(MsgJsonHelper.DebugJson("Login接口请求异常"));
-    });
+	let id = req.body.ID;
+	let pwd = req.body.PWD;
+	let captcha = req.body.CAPTCHA;
+	let _captcha = req.body._CAPTCHA;
+	if (!id || !pwd || !captcha || !_captcha) {
+		return res.json(MsgJsonHelper.DebugJson('参数异常'));
+	}
+	if (jsmd5(jsmd5.digest(captcha.toLowerCase())) !== _captcha) {
+		return res.json(MsgJsonHelper.DebugJson('验证码错误'));
+	}
+	let condition = [];
+	condition.push(
+		new MemoryCondition({
+			Field: 'ZK_ID',
+			Logic: MLogic.And,
+			Operator: MOperator.Equal,
+			Type: MType.Mstring,
+			value: id,
+		})
+	);
+	condition.push(
+		new MemoryCondition({
+			Field: 'ZK_PASSWORD',
+			Logic: MLogic.And,
+			Operator: MOperator.Equal,
+			Type: MType.Mstring,
+			value: pwd,
+		})
+	);
+	client
+		.Query(QueryModel.ZK_USERINFO, condition, null, 0, 0, false, null)
+		.then(m => {
+			if (m.result.length === 0) {
+				res.json(MsgJsonHelper.DebugJson('账号密码错误'));
+			} else {
+				VolidRole(m.result[0], res);
+			}
+		})
+		.catch(err => {
+			res.json(MsgJsonHelper.DebugJson('Login接口请求异常'));
+		});
 }
 
 /**
@@ -81,16 +91,11 @@ function Login(req, res) {
  * @param {*} res
  */
 function Verifylogin(req, res) {
-  let user = req.UserInfo;
-  if (user) {
-    user["ZK_PASSWORD"] = user["ZK_PASSWORD"].replace(
-      /^(.{5})(.+)(.{5})$/,
-      "$1*****$3"
-    );
-  }
-  res.json(
-    MsgJsonHelper.DefaultJson(user, user === undefined ? false : true, "")
-  );
+	let user = req.UserInfo;
+	if (user) {
+		user['ZK_PASSWORD'] = user['ZK_PASSWORD'].replace(/^(.{5})(.+)(.{5})$/, '$1*****$3');
+	}
+	res.json(MsgJsonHelper.DefaultJson(user, user === undefined ? false : true, ''));
 }
 
 /**
@@ -99,7 +104,7 @@ function Verifylogin(req, res) {
  * @param {*} res
  */
 function Logout(req, res) {
-  res.json(MsgJsonHelper.DefaultJson("", true, "请清空登录票据"));
+	res.json(MsgJsonHelper.DefaultJson('', true, '请清空登录票据'));
 }
 
 /**
@@ -108,35 +113,33 @@ function Logout(req, res) {
  * @param {*} res
  */
 function VolidRole(userinfo, res) {
-  let condition = [];
-  condition.push(
-    new MemoryCondition({
-      Field: "ZK_ID",
-      Logic: MLogic.And,
-      Operator: MOperator.Equal,
-      Type: MType.Mstring,
-      value: userinfo.ZK_ROLE
-    })
-  );
-  client
-    .Query(QueryModel.ZK_ROLEINFO, condition, null, 0, 0, false, null)
-    .then(m => {
-      if (m.result.length === 0) {
-        res.json(MsgJsonHelper.DebugJson("角色异常"));
-      } else {
-        let role = m.result[0];
-        if (role.ZK_ISADMIN == "1") {
-          SavePermits(userinfo, res);
-        } else {
-          res.json(
-            MsgJsonHelper.DebugJson("您还不是管理员，请联系管理员升级权限")
-          );
-        }
-      }
-    })
-    .catch(err => {
-      res.json(MsgJsonHelper.DebugJson("VolidRole接口请求异常"));
-    });
+	let condition = [];
+	condition.push(
+		new MemoryCondition({
+			Field: 'ZK_ID',
+			Logic: MLogic.And,
+			Operator: MOperator.Equal,
+			Type: MType.Mstring,
+			value: userinfo.ZK_ROLE,
+		})
+	);
+	client
+		.Query(QueryModel.ZK_ROLEINFO, condition, null, 0, 0, false, null)
+		.then(m => {
+			if (m.result.length === 0) {
+				res.json(MsgJsonHelper.DebugJson('角色异常'));
+			} else {
+				let role = m.result[0];
+				if (role.ZK_ISADMIN == '1') {
+					SavePermits(userinfo, res);
+				} else {
+					res.json(MsgJsonHelper.DebugJson('您还不是管理员，请联系管理员升级权限'));
+				}
+			}
+		})
+		.catch(err => {
+			res.json(MsgJsonHelper.DebugJson('VolidRole接口请求异常'));
+		});
 }
 
 /**
@@ -145,37 +148,54 @@ function VolidRole(userinfo, res) {
  * @param {*} res
  */
 function SavePermits(userinfo, res) {
-  let condition = [];
-  condition.push(
-    new MemoryCondition({
-      Field: "ZK_ROLE",
-      Logic: MLogic.And,
-      Operator: MOperator.Equal,
-      Type: MType.Mstring,
-      value: userinfo.ZK_ROLE
-    })
-  );
-  client
-    .Query(QueryModel.ZK_PERMITCONFIG, condition, null, 0, 0, false, null)
-    .then(m => {
-      let permitIds = [];
-      m.result.forEach(permit => {
-        permitIds.push(permit.ZK_PERMITID);
-      });
-      res.json(
-        MsgJsonHelper.DefaultJson(
-          {
-            accessToken: TokenHelper.set(userinfo),
-            permitToken: TokenHelper.set(m.result)
-          },
-          true,
-          ""
-        )
-      );
-    })
-    .catch(err => {
-      res.json(MsgJsonHelper.DebugJson("SavePermits接口请求异常"));
-    });
+	let condition = [];
+	condition.push(
+		new MemoryCondition({
+			Field: 'ZK_ROLE',
+			Logic: MLogic.And,
+			Operator: MOperator.Equal,
+			Type: MType.Mstring,
+			value: userinfo.ZK_ROLE,
+		})
+	);
+	client
+		.Query(QueryModel.ZK_PERMITCONFIG, condition, null, 0, 0, false, null)
+		.then(m => {
+			let permitIds = [];
+			m.result.forEach(permit => {
+				permitIds.push(permit.ZK_PERMITID);
+			});
+			res.json(
+				MsgJsonHelper.DefaultJson(
+					{
+						accessToken: TokenHelper.set(userinfo),
+						permitToken: TokenHelper.set(m.result),
+					},
+					true,
+					''
+				)
+			);
+		})
+		.catch(err => {
+			res.json(MsgJsonHelper.DebugJson('SavePermits接口请求异常'));
+		});
+}
+
+/**
+ * 生成图片二维码
+ * @param {*} req
+ * @param {*} res
+ */
+function Createcaptcha(req, res) {
+	var captcha = svgCaptcha.create({
+		size: 4,
+		noise: 2,
+		color: true,
+		width: 150,
+		height: 50,
+		background: 'transparent',
+	});
+	res.json(MsgJsonHelper.DefaultJson(captcha.data, true, jsmd5(jsmd5.digest(captcha.text.toLowerCase()))));
 }
 
 module.exports = router;
